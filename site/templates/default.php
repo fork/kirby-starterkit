58ab3ad3a6cc6bb112181e9a5c7e9266a7653f4a
Holger Meyer <holger.meyer@fork.de>
2023-03-30 21:42:16 +0200

    feat(seo): Added seo information to site blueprint and layout

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,8 @@
-<h1><?= $page->title() ?></h1>
+<html>
+  <head>
+    <?php snippet('meta_information'); ?>
+    <?php snippet('robots'); ?>
+  </head>
+  <body><h1><?= $page->title() ?></h1></body>
+</html>
+
