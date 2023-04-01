2f3bbe5e17207a4cb6ec6fe029ee8f60c61b82d5
Holger Meyer <holger.meyer@fork.de>
2023-04-01 20:31:56 +0200

    feat(header): Add Iconset

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,7 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <meta name="viewport" content="width=device-width"/>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+
   <?php snippet('meta_information'); ?>
   <?php snippet('robots'); ?>
+
+  <link rel="icon" type="image/svg+xml" href="/assets/icons/favicon.svg">
+  <link rel="icon" type="image/png" sizes="192x192" href="/assets/icons/android-icon-192x192.png"/>
+  <link rel="icon" type="image/png" sizes="32x32" href="/assets/icons/favicon-32x32.png"/>
+  <link rel="icon" type="image/png" sizes="96x96" href="/assets/icons/favicon-96x96.png"/>
+  <link rel="icon" type="image/png" sizes="16x16" href="/assets/icons/favicon-16x16.png"/>
+  <link rel="apple-touch-icon" sizes="180x180" href="/assets/icons/apple-icon-180x180.png"/>
+  
+  <link rel="manifest" href="/assets/icons/manifest.json"/>
+
+  <meta name="msapplication-TileColor" content="#000000"/>
+  <meta name="theme-color" content="#000000"/>
 </head>
 <body>
