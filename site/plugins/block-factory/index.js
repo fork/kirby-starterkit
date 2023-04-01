109687c4e022fb81d04da9cbb9d633a0c15775cd
Holger Meyer <holger.meyer@fork.de>
2023-04-01 21:33:18 +0200

    feat(block): Added nesting to accordion block

--- site/plugins/block-factory/index.js
+++ site/plugins/block-factory/index.js
@@ -1,16 +1,15 @@
 panel.plugin("cookbook/block-factory", {
   blocks: {
     accordion: `
-      <div>
-        <div v-if="content.summary">
-          <details>
-            <summary>{{ content.summary }}</summary>
-            <div v-if="content.details" v-html="content.details"></div>
+      <div @dblclick="open">
+        <h2 class="k-block-type-accordion-heading" v-html="content.headline"></h2>
+        <div v-if="content.accordion.length">
+          <details v-for="(item, index) in content.accordion" class="k-block-type-accordion-item" :key="index">
+            <summary>{{ index + item.summary }}</summary>
+            <div v-html="item.details"></div>
           </details>
         </div>
-        <div v-else>
-          No content yet
-        </div>
+        <div v-else>No items yet</div>
       </div>
     `,
   },
