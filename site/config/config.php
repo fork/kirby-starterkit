17e217b754288a873f3b1d4f3dd7fceaf8643cc0
Holger Meyer <holger.meyer@fork.de>
2023-04-02 00:11:46 +0200

    feat: Added sitemap snippet and routes

--- site/config/config.php
+++ site/config/config.php
@@ -4,10 +4,33 @@ return [
   'hooks' => [],
   'routes' => [
     [
+      // articles overview is not a page itself but just a container
       'pattern' => 'articles',
       'action' => fn () => false
+    ],
+    [
+      'pattern' => 'sitemap.xml',
+      'action'  => function () {
+          $pages = site()->pages()->index();
+
+          // fetch the pages to ignore from the config settings,
+          // if nothing is set, we ignore the error page
+          $ignore = kirby()->option('sitemap.ignore', ['error']);
+
+          $content = snippet('sitemap', compact('pages', 'ignore'), true);
+
+          // return response with correct header type
+          return new Kirby\Cms\Response($content, 'application/xml');
+      }
+    ],
+    [
+      'pattern' => 'sitemap',
+      'action'  => function () {
+          return go('sitemap.xml', 301);
+      }
     ]
   ],
+  'sitemap.ignore' => ['error'],
   'cache' => [
     'pages' => [
       'active' => true,
---
+++ site/snippets/sitemap.php
@@ -0,0 +1,13 @@
+<?= '<?xml version="1.0" encoding="utf-8"?>'; ?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <?php foreach ($pages as $p): ?>
+        <?php if (in_array($p->uri(), $ignore)) {
+            continue;
+        } ?>
+        <url>
+            <loc><?= html($p->url()) ?></loc>
+            <lastmod><?= $p->modified('c', 'date') ?></lastmod>
+            <priority><?= ($p->isHomePage()) ? 1 : number_format(0.5 / $p->depth(), 1) ?></priority>
+        </url>
+    <?php endforeach ?>
+</urlset>
