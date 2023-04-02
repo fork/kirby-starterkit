9c9fe26da5d3b75b013f0dd651fee5730369741c
Holger Meyer <holger.meyer@fork.de>
2023-04-03 00:07:40 +0200

    feature: Added Alpine and disabled pnp i n favor of TypeScript Types

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-import "./style.css";
+import './style.css';
+
+import Alpine from 'alpinejs';
+
+// suggested in the Alpine docs:
+// make Alpine on window available for better DX
+window.Alpine = Alpine;
+
+Alpine.start();
