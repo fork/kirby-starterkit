7160c4fe9505d3facad40fbb7eff64b96747d92f
Holger Meyer <holger.meyer@fork.de>
2024-01-12 13:43:58 +0100

    fix: Use customized blocks for home

--- site/snippets/blocks.php
+++ site/snippets/blocks.php
@@ -2,4 +2,4 @@
   <?php foreach ($page->blocks()->toBlocks() as $block) : ?>
     <?php snippet('blocks/'.$block->type(), ['block' => $block]) ?>
   <?php endforeach ?>
-</div>
+</div><?php var_dump($page->blocks()->toBlocks()) ?>
